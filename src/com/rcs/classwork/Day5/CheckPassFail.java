e25c2c73dd54c3368448501e34479bd075bf27cf
KruumsLV <lvkrumins97@gmail.com>
2021-02-27 11:23:10 +0200

    Classword tasks

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -1,13 +1,20 @@
 package com.rcs.classwork.Day5;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	    // write your code here
-        //
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ievadiet x :");
+        int x = scanner.nextInt();
+        scanner.close();
+        if (x >= 50) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+        System.out.println("DONE");
     }
 }
+
---
+++ src/com/rcs/classwork/Day5/OddOrEven.java
@@ -0,0 +1,9 @@
+package com.rcs.classwork.Day5;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        System.out.println("Uzdevums 2");
+        // write your code here
+        //
+    }
+}
