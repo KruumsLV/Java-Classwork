ee4b8fbd8c0cb449e6a4d491e44e1b8b670d0041
Kristaps Krumins <kristaps.krumins@lightsoft.lv>
2021-03-29 20:02:26 +0300

    Day17 progress

---
+++ src/com/rcs/classwork/Day17/Circle.java
@@ -0,0 +1,22 @@
+package com.rcs.classwork.Day17;
+
+public class Circle {
+    // Constant PI
+    static final double PI = 3.14159265;
+
+    // Lauki:
+    double radius;
+    String color;
+
+    // 2 konstruktori
+    // 1. -> 1 parametru => radius
+    //       color = "Black";
+    // 2. -> 2 parametriem => radius, color
+
+    // Metodes
+    // getArea() -> kas aprēķina un atgriež apļa laukumu
+    // PI*r^2
+    // getCircleLength() -> kas aprēķina un atgriež riņķa
+    // līnijas garumu
+    // 2*PI*r
+}
---
+++ src/com/rcs/classwork/Day17/Main.java
@@ -0,0 +1,18 @@
+package com.rcs.classwork.Day17;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Circle redCircle = new Circle(6.4, "Red");
+        // Circle blueCircle = new Circle(2.0, "Blue");
+        // Circle blackCircle = new Circle(3.2);
+
+        // ...
+        // redCircle.getArea()
+        // redCircle.getCircleLength()
+
+        // redCircle -> Color = Red, Area = ??, Circle Length = ??
+        // blueCircle -> ...
+        // blackCircle ->
+    }
+}
