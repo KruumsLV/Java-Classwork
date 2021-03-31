15266ca411534e6351bed9133ccf342263444ae7
Kristaps Krumins <kristaps.krumins@lightsoft.lv>
2021-03-31 23:06:40 +0300

    Day18

--- src/com/rcs/classwork/Day17/Circle.java
+++ src/com/rcs/classwork/Day17/Circle.java
@@ -3,20 +3,68 @@ package com.rcs.classwork.Day17;
 public class Circle {
     // Constant PI
     static final double PI = 3.14159265;
+    static final String defaultColor = "Black";
 
     // Lauki:
-    double radius;
-    String color;
+    private double radius;
+    private String color;
 
     // 2 konstruktori
-    // 1. -> 1 parametru => radius
-    //       color = "Black";
-    // 2. -> 2 parametriem => radius, color
+    /**
+     * Konstruktors ar 1 parametru.
+     * Krāsa tiek uzstādīta Circle.defaultColor
+     * @param radius Circle radius
+     */
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = defaultColor;
+    }
+
+    /**
+     * Konstruktors ar 2 parametriem.
+     * @param radius Rādiuss
+     * @param color Krāsa
+     */
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius > 0) {
+            this.radius = radius;
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
 
     // Metodes
-    // getArea() -> kas aprēķina un atgriež apļa laukumu
-    // PI*r^2
-    // getCircleLength() -> kas aprēķina un atgriež riņķa
-    // līnijas garumu
-    // 2*PI*r
+    /**
+     * Aprēķina riņķa laukumu
+     * @return Riņķa laukuma vērtību
+     */
+    public double getArea() {
+        //return Math.pow(this.radius, 2.0) * PI;
+        return this.radius * this.radius * PI * 2;
+    }
+
+    /**
+     * Aprēķina riņķa līnijas garumu
+     * @return Riņķa līnijas garuma vērtību
+     */
+    public double getCircleLength() {
+        return 2 * PI * this.radius;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Color = %s, Area = %.2f, Circle Length = %.2f\n",
+                            this.color, this.getArea(), this.getCircleLength());
+    }
 }
--- src/com/rcs/classwork/Day17/Main.java
+++ src/com/rcs/classwork/Day17/Main.java
@@ -1,18 +1,29 @@
 package com.rcs.classwork.Day17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Circle redCircle = new Circle(6.4, "Red");
-        // Circle blueCircle = new Circle(2.0, "Blue");
-        // Circle blackCircle = new Circle(3.2);
+        //System.out.println(Circle.defaultColor);
 
-        // ...
-        // redCircle.getArea()
-        // redCircle.getCircleLength()
+/*        Circle[] circleArray = new Circle[3];
+        circleArray[0] = new Circle(3.34, "Red");;
+        circleArray[1] = new Circle(2.0);;
+        circleArray[2] = new Circle(5.23, "Blue");*/
 
-        // redCircle -> Color = Red, Area = ??, Circle Length = ??
-        // blueCircle -> ...
-        // blackCircle ->
+        List<Circle> circleList = new ArrayList<Circle>();
+        circleList.add(new Circle(3.34, "Red"));
+        circleList.add(new Circle(2.0));
+        circleList.add(new Circle(5.23, "Blue"));
+
+        for(Circle circle : circleList) {
+            System.out.print(circle);
+        }
+
+/*        System.out.print(redCircle);
+        System.out.print(blackCircle);
+        System.out.print(blueCircle);*/
     }
 }
